56113766356a759b57df08782b64742b660e2df2
immorrtalz <61843974+immorrtalz@users.noreply.github.com>
2024-10-26 15:58:57 +0500

    26102024-1558

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,28 @@
 import { convertFileSrc } from '@tauri-apps/api/core';
 import { getMatches } from '@tauri-apps/plugin-cli';
+import { platform } from '@tauri-apps/plugin-os';
 import { getCurrentWindow } from '@tauri-apps/api/window';
-import { appConfigDir, dataDir } from '@tauri-apps/api/path';
-import { exists, mkdir, readTextFile, writeTextFile } from '@tauri-apps/plugin-fs';
+import { appConfigDir, dataDir, join } from '@tauri-apps/api/path';
+import { exists, mkdir, readDir, readTextFile, writeTextFile, BaseDirectory } from '@tauri-apps/plugin-fs';
 /* import { message, ask, open } from '@tauri-apps/plugin-dialog'; //npm i @tauri-apps/plugin-dialog --save */
-import { listen } from '@tauri-apps/api/event';
+import { EventCallback, listen } from '@tauri-apps/api/event';
 import { getVersion } from '@tauri-apps/api/app';
 
 /* import { shell_open } from '@tauri-apps/api/open'; */
 
-/* const trackNameContainer = document.querySelector('.currently-playing-track-name-container');
-const trackName = document.querySelector('.currently-playing-track-name');
-const trackControlsPause = document.querySelector('.currently-playing-pause-button');
-
-const timeTexts = document.querySelector('.time-texts');
-const timeText1 = document.querySelector('#time-text-1');
-const timeText2 = document.querySelector('#time-text-2');
-
-const trackTimelineFront = document.querySelector('.track-timeline-front'); */
-
-/* const timelineHandle = document.querySelector('.track-timeline-handle-visual'); */
-
-/* const musicPlayer = document.querySelector('#music');
-const accentColorButtons = document.querySelectorAll('.settings-param-color');
-const clearPlaylistOnNewFileCheckbox = document.getElementById('clearPlaylistOnNewFileCheckbox');
-const equalizerInputs = document.querySelectorAll('.equalizer-bar-front');
-const equalizerBarHandles = document.querySelectorAll('.equalizer-bar-handle-visual');
-const playlistTracksContainer = document.getElementById('playlistTracksContainer'); */
-
 const appWindow = getCurrentWindow();
 const isDev = false;
 const supportedExtensions = [ 'mp3', 'wav', 'weba', 'webm', 'm4a', 'ogg', 'oga', 'caf', 'flac', 'opus', 'mid', 'aiff', 'wma', 'au' ];
+var musicPlayer : HTMLAudioElement;
 
 const mainContentWrapper = getElement('#main-content-wrapper');
-
 const allPages = getElementsByClassName('page');
-
 const backBtn = getElement('#backBtn');
 const pageNameTexts = getElementsByClassName('page-name');
 
 const sideMenu = getElement('.side-menu');
 const sideMenuBg = getElement('.side-menu-bg');
 const sideMenuContainer = getElement('.side-menu-container');
-
 const openEqualizerPageBtn = getElement('#openEqualizerPageBtn');
 const openSettingsPageBtn = getElement('#openSettingsPageBtn');
 
@@ -58,15 +38,49 @@ var isPlayAfterTimelineValueChange = false;
 var isTimelineValueInput = false;
 var timelineValueInputTimeoutId; */
 
+class Settings
+{
+	accentColor = 16;
+	eq = [0, 0, 0, -2.1, -2.2, -2, -2.1, -2.1, -1.8, -0.6];
+	library = [];
+
+	constructor() { this.resetEQ(); }
+	resetEQ() { this.eq = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]; }
+}
+
+const settings = new Settings();
+var settingsFilePath = '';
+
+/* const platformResult = await platform();
+if (platformResult !== 'android') return; */
+
+/* const matches = await getMatches().catch(() => { console.log('error in getMatches'); });
+const filePath = matches.args.filePath.value; */
+
+/* if (!isDev) await validateFilePath(filePath);
+addAudioFileToPlaylist(filePath);
+playAudioFile(playlist[0]); */
+
 document.addEventListener('DOMContentLoaded', () =>
 {
+	musicPlayer = <HTMLAudioElement> document.getElementById('music');
+
 	getElement('#openSideMenuBtn').addEventListener('click', () => openOrCloseSideMenu(0));
 	sideMenuBg.addEventListener('click', () => openOrCloseSideMenu(1));
 
 	getElement('#openPlaylistPageBtn').addEventListener('click', () => openPage(1));
 	openEqualizerPageBtn.addEventListener('click', () => { openPage(2); removeRipplesInChildren(openEqualizerPageBtn); });
-	/* openSettingsPageBtn.addEventListener('click', () => { openPage(3); removeRipplesInChildren(openSettingsPageBtn); }); */
+	openSettingsPageBtn.addEventListener('click', () => { openPage(3); removeRipplesInChildren(openSettingsPageBtn); });
+
+	getElement('#testBtn').addEventListener('click', () => addMusicFilesLocation());
 });
+
+async function addMusicFilesLocation()
+{
+	const entries = await readDir('', { baseDir: BaseDirectory.Home });
+	console.log(entries);
+	console.log(entries[0]); //doesn't work
+}
 
 function hideOrRevealElement(element : HTMLElement, hideOrReveal : number)
 {
@@ -103,7 +117,7 @@ function openOrCloseSideMenu(openOrClose : number)
 		case 0:
 			if (isSideMenuOpened) return;
 
-			clearTimeout(sideMenuCloseTimeoutId);
+			if (sideMenuCloseTimeoutId !== undefined && sideMenuCloseTimeoutId !== null) clearTimeout(sideMenuCloseTimeoutId);
 
 			isSideMenuOpened = true;
 			sideMenu.style.display = 'unset';
@@ -151,7 +165,7 @@ function openPage(nextPageIndex : number)
 
 	hideOrRevealElement(backBtn, Number(nextPageIndex != 0));
 	eval(`backBtn.${nextPageIndex == 0 ? 'remove' : 'add'}EventListener('click', () => openPage(0));`);
-	if (isSideMenuOpened && nextPageIndex == 2) openOrCloseSideMenu(1);
+	if (isSideMenuOpened && (nextPageIndex == 2 || nextPageIndex == 3)) openOrCloseSideMenu(1);
 
 	const pageNames = ['main', 'playlist', 'equalizer', 'settings'];
 
@@ -166,6 +180,7 @@ function openPage(nextPageIndex : number)
 	prevPage.style.opacity = '0';
 	prevPage.style.transform = 'scale(0.9)';
 	prevPage.style.zIndex = '-1';
+	prevPage.style.pointerEvents = 'none';
 	nextPage.style.display = 'unset !important';
 
 	setTimeout(() =>
@@ -179,6 +194,7 @@ function openPage(nextPageIndex : number)
 	{
 		prevPage.style.display = 'none';
 		prevPage.style.transform = 'scale(1.1)';
+		nextPage.style.pointerEvents = 'all';
 		canTransitionBetweenPages = true;
 	}, 300);
 }
